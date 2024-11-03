a5958c4552540062a4a0c0bd54e849927f2e200a
Rus <rus.zakirov@triosstudent.com>
2024-11-03 11:15:21 -0500

    Added a working directory from the Angular_Assignment_1

---
+++ ReservationSystem/data.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  return {
+    services: [
+      { id: 1, name: "Area-1", session: "Morning", description: "A session from 9am-12pm" },
+      { id: 2, name: "Area-1", session: "Noon", description: "A session from 12pm-3pm" },
+      { id: 3, name: "Area-1", session: "Afternoon", description: "A session from 3pm-6pm" },
+      { id: 4, name: "Area-2", session: "Morning", description: "A session from 9am-12pm" },
+      { id: 5, name: "Area-2", session: "Noon", description: "A session from 12pm-3pm" },
+      { id: 6, name: "Area-2", session: "Afternoon", description: "A session from 3pm-6pm" },
+      { id: 7, name: "Area-3", session: "Morning", description: "A session from 9am-12pm" },
+      { id: 8, name: "Area-3", session: "Noon", description: "A session from 12pm-3pm" },
+      { id: 9, name: "Area-3", session: "Afternoon", description: "A session from 3pm-6pm" },
+      { id: 10, name: "Area-4", session: "Morning", description: "A session from 9am-12pm" },
+      { id: 11, name: "Area-4", session: "Noon", description: "A session from 12pm-3pm" },
+      { id: 12, name: "Area-4", session: "Afternoon", description: "A session from 3pm-6pm" },
+    ],
+    reservations: []
+  }
+}
