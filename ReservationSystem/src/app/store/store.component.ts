4b8c6bae2dabc274e1d3e5f06508c1f414c83bd2
Rus <rus.zakirov@triosstudent.com>
2024-11-03 23:10:05 -0500

    Added pagination

--- ReservationSystem/src/app/store/store.component.ts
+++ ReservationSystem/src/app/store/store.component.ts
@@ -2,16 +2,40 @@ import { Component } from "@angular/core";
 import { Service } from "../model/service.model";
 import { ServiceRepository } from "../model/service.repository";
 @Component({
- selector: "store",
- templateUrl: "store.component.html"
+  selector: "store",
+  templateUrl: "store.component.html"
 })
 export class StoreComponent {
- constructor(private repository: ServiceRepository) { }
- get services(): Service[] {
-  return this.repository.getServices();
- }
+  selectedSession: string | undefined;
+  sessionsPerPage = 4;
+  selectedPage = 1;
 
- get sessions(): string[] {
-  return this.repository.getSessions();
+  constructor(private repository: ServiceRepository) { }
+
+  get services(): Service[] {
+    let pageIndex = (this.selectedPage - 1) * this.sessionsPerPage
+    return this.repository.getServices(this.selectedSession)
+    .slice(pageIndex, pageIndex + this.sessionsPerPage);
   }
- }
+
+  get sessions(): string[] {
+    return this.repository.getSessions();
+  }
+
+  changeSession(newSession?: string) {
+    this.selectedSession = newSession;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+    }
+    changePageSize(newSize: number) {
+    this.sessionsPerPage = Number(newSize);
+    this.changePage(1);
+    }
+    get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+    .getServices(this.selectedSession).length / this.sessionsPerPage))
+    .fill(0).map((x, i) => i + 1);
+    }
+}
