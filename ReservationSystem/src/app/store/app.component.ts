f3ee05268379c3d81f3418d596059e4866df7d99
Rus <rus.zakirov@triosstudent.com>
2024-11-03 22:22:06 -0500

    Modfied multile files to render app's services with details

--- ReservationSystem/src/app/store/app.component.ts
+++ ReservationSystem/src/app/store/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { StoreModule } from '../store/store.module';
 
 @Component({
   selector: 'app',
   standalone: true,
-  imports: [],
+  imports: [StoreModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ReservationSystem';
