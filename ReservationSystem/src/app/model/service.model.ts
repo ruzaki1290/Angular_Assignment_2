e77ef985223b649348dafc8e4757c3f024df1c41
Rus <rus.zakirov@triosstudent.com>
2024-11-04 00:40:05 -0500

    Added a cart.model.ts

--- ReservationSystem/src/app/model/service.model.ts
+++ ReservationSystem/src/app/model/service.model.ts
@@ -1,5 +1,6 @@
 // This will hold our data model for a service and keep a track of id, name, session and a price.
 export class Service {
+  static id: any;
   constructor(
   public id?: number,
   public name?: string,
