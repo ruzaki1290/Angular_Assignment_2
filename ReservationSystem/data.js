6a6aec6d4697b1b1d654cbb99e2d67da56860731
Rus <rus.zakirov@triosstudent.com>
2024-11-03 20:02:08 -0500

    Modified data.js file to include the price for each area

--- ReservationSystem/data.js
+++ ReservationSystem/data.js
@@ -1,18 +1,18 @@
 module.exports = function () {
   return {
     services: [
-      { id: 1, name: "Area-1", session: "Morning", description: "A session from 9am-12pm" },
-      { id: 2, name: "Area-1", session: "Noon", description: "A session from 12pm-3pm" },
-      { id: 3, name: "Area-1", session: "Afternoon", description: "A session from 3pm-6pm" },
-      { id: 4, name: "Area-2", session: "Morning", description: "A session from 9am-12pm" },
-      { id: 5, name: "Area-2", session: "Noon", description: "A session from 12pm-3pm" },
-      { id: 6, name: "Area-2", session: "Afternoon", description: "A session from 3pm-6pm" },
-      { id: 7, name: "Area-3", session: "Morning", description: "A session from 9am-12pm" },
-      { id: 8, name: "Area-3", session: "Noon", description: "A session from 12pm-3pm" },
-      { id: 9, name: "Area-3", session: "Afternoon", description: "A session from 3pm-6pm" },
-      { id: 10, name: "Area-4", session: "Morning", description: "A session from 9am-12pm" },
-      { id: 11, name: "Area-4", session: "Noon", description: "A session from 12pm-3pm" },
-      { id: 12, name: "Area-4", session: "Afternoon", description: "A session from 3pm-6pm" },
+      { id: 1, name: "Area-1", session: "Morning", description: "A session from 9am-12pm", price: 100 },
+      { id: 2, name: "Area-1", session: "Noon", description: "A session from 12pm-3pm", price: 75 },
+      { id: 3, name: "Area-1", session: "Afternoon", description: "A session from 3pm-6pm", price: 50 },
+      { id: 4, name: "Area-2", session: "Morning", description: "A session from 9am-12pm", price: 150 },
+      { id: 5, name: "Area-2", session: "Noon", description: "A session from 12pm-3pm", price: 125 },
+      { id: 6, name: "Area-2", session: "Afternoon", description: "A session from 3pm-6pm", price: 100 },
+      { id: 7, name: "Area-3", session: "Morning", description: "A session from 9am-12pm", price: 150 },
+      { id: 8, name: "Area-3", session: "Noon", description: "A session from 12pm-3pm", price: 125 },
+      { id: 9, name: "Area-3", session: "Afternoon", description: "A session from 3pm-6pm", price: 100 },
+      { id: 10, name: "Area-4", session: "Morning", description: "A session from 9am-12pm", price: 200 },
+      { id: 11, name: "Area-4", session: "Noon", description: "A session from 12pm-3pm", price: 175 },
+      { id: 12, name: "Area-4", session: "Afternoon", description: "A session from 3pm-6pm", price: 150 },
     ],
     reservations: []
   }
