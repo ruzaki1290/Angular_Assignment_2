46933e4e8ee88e19eaed24eaf5564c00c2453038
Rus <rus.zakirov@triosstudent.com>
2024-11-04 01:05:27 -0500

    Added multiple files for cart, but ran into some issues implementing templateUrl, associated with the path

--- ReservationSystem/src/app/model/model.module.ts
+++ ReservationSystem/src/app/model/model.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
 import { ServiceRepository } from "./service.repository";
 import { StaticDataSource } from "./static.datasource";
+import { Cart } from "./cart.model";
 @NgModule({
- providers: [ServiceRepository, StaticDataSource]
+ providers: [ServiceRepository, StaticDataSource, Cart]
 })
 export class ModelModule { }
--- ReservationSystem/src/app/store/store.module.ts
+++ ReservationSystem/src/app/store/store.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { CounterDirective } from "./counter.directive";
+import { CartSummaryComponent } from "./cartSummary.component";
 @NgModule({
  imports: [ModelModule, CommonModule, FormsModule],
  declarations: [StoreComponent, CounterDirective],
